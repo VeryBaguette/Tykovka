a0643aabcfa51629d141472eab685352e0c454e3
Baguette <zhanibek-2005@bk.ru>
2021-08-10 17:17:57 +0600

    123

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var modal = $(".modal"),
+    modalBtn = $("[data-toggle=modal]"),
+    closeBtn = $(".modal__close");
+  modalBtn.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
+
+  closeBtn.on("click", function () {
+    modal.toggleClass("modal--visible");
+  });
+  console.log("HELLLLO!!!!");
+});
